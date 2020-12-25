f54de32c614d287123873ee42e81c8bc402bc011
Ariel_Ibarra <arielibarra39@gmail.com>
2020-12-25 09:51:43 -0800

    commit tree ae1dd5c1f3be84ed2d3a27e997a2be746d094e80

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, callback, start) {
+    if(!!start === false) {
+        start = array.shift()
+    }
+    return array.reduce(callback, start)
+}
